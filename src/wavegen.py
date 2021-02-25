b7ebd250804e8991089e77542eb280380876e475
Saverio Monaco <saveriomonaco97@gmail.com>
2021-02-25 23:15:59 +0100

    python script

--- src/wavegen.py
+++ src/wavegen.py
@@ -1,9 +1,15 @@
+import serial
+ser = serial.Serial('/dev/ttyUSB21', baudrate=115200)
+
+# Let's load the array from the file 'input.txt'
+file_in  = open('./src/input.txt', 'r')
+file_ou  = open('./src/input.txt', 'w')
+
 def signed_to_unsigned(signed):
     if(signed>=0):
         return signed
     else:
         return 2**8+signed
-
 
 def unsigned_to_signed(unsigned):
     if(unsigned<2**7):
@@ -11,25 +17,15 @@ def unsigned_to_signed(unsigned):
     else:
         return unsigned-2**8
 
-
 def int_to_char(value):
     return chr(signed_to_unsigend(value))
-
 
 def char_to_int(character):
     return unsigned_to_signed(ord(character))
 
-import serial
-ser = serial.Serial('/dev/ttyUSB21', baudrate=115200)
-
-# Let's load the array from the file 'input.txt'
-file_in = open('./src/input.txt', 'r')
-file_ou  = open('./src/input.txt', 'w')
-
 for i in range(100):
     sig = file_in.readline()
-    ser.write(chr(signed_to_unsigend(sig)))
+    ser.write(int_to_char(sig))
     d = ser.read()
-    #print("Output:", ord(d))
-    print(unsigned_to_signed(ord(d)))
-    file_ou.write('%d\n' % unsigned_to_signed(ord(d)).astype(int))
+    print(char_to_int(d))
+    file_ou.write('%d\n' % char_to_int(d))
