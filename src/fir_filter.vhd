0b82f1dbb96cdb1148344363d38048e9b856a71a
Saverio Monaco <saveriomonaco97@gmail.com>
2021-02-25 16:12:39 +0100

    testing UART

--- src/fir_filter.vhd
+++ src/fir_filter.vhd
@@ -171,8 +171,9 @@ architecture rtl of fir_filter_4 is
         if(fir_rstb='0') then
           fir_o_data     <= (others=>'0');
         elsif(rising_edge(fir_clk)) then
-          if(fir_i_valid='1') then
+          if(fir_i_valid = '1') then
             fir_o_data     <= std_logic_vector(r_add_st1(17 downto 8)); -- here
+            fir_o_valid = '1'
           end if;
         end if;
     end process p_output;
