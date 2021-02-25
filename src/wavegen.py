4b95e34ec0932075fd843c4da0e78b738b6a53fc
Saverio Monaco <saveriomonaco97@gmail.com>
2021-02-25 23:25:00 +0100

    python script

--- src/wavegen.py
+++ src/wavegen.py
@@ -24,7 +24,7 @@ def char_to_int(character):
     return unsigned_to_signed(ord(character))
 
 for i in range(100):
-    sig = file_in.readline()
+    sig = int(file_in.readline())
     ser.write(int_to_char(sig))
     d = ser.read()
     print(char_to_int(d))
