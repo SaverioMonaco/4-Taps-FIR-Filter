b47b273ccc4827ba4f1bc193d1a72a429435bbc5
Saverio Monaco <saveriomonaco97@gmail.com>
2021-02-26 10:13:40 +0100

    testing python script

--- src/fir.py
+++ src/fir.py
@@ -2,9 +2,6 @@ import serial
 import math
 ser = serial.Serial('/dev/ttyUSB21', baudrate=115200)
 
-# Let's load the array from the file 'input.txt'
-file_ou  = open('./src/input.txt', 'w')
-
 def signed_to_unsigned(signed):
     if(signed>=0):
         return signed
@@ -39,5 +36,4 @@ print("-------------------\nOutput:")
 for i in range(data_size):
     ser.write(int_to_char(sig_data[i]))
     d = ser.read()
-    print(char_to_int(d)*8)
-    file_ou.write('%d\n' % 32*char_to_int(d))
+    print(char_to_int(d)*32)
