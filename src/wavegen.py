5180bd4609091d6aa4830f4a6335157162a6dae7
Saverio Monaco <saveriomonaco97@gmail.com>
2021-02-26 15:40:40 +0100

    first working version

--- src/wavegen.py
+++ src/wavegen.py
@@ -1,12 +1,39 @@
+import serial
 import math
+ser = serial.Serial('/dev/ttyUSB21', baudrate=115200)
 
-data_size = 100
-noise = 1 # the higher the noisier
+def signed_to_unsigned(signed):
+    if(signed>=0):
+        return signed
+    else:
+        return 2**8+signed
 
-# we now open the file input_vectors.txt so that the vhd code can process it
-f = open("input.txt", "w")
+def unsigned_to_signed(unsigned):
+    if(unsigned<2**7):
+        return unsigned
+    else:
+        return unsigned-2**8
 
+def int_to_char(value):
+    return chr(signed_to_unsigned(value))
+
+def char_to_int(character):
+    return unsigned_to_signed(ord(character))
+
+sig_data = []
+
+data_size = 200
+noise = 1# the higher the noisier
+print("-------------------\nInput:")
 for i in range(data_size):
-    sig = 60*(math.sin(i/10)+ noise)
+    sig = int(62*(math.sin(i/10)+ noise))
+    print(sig)
     noise = -noise
-    f.write('%d\n' % int(sig)) # we write it as an int
+    sig_data.append(sig)
+
+print("-------------------\nOutput:")
+# wavegen
+for i in range(data_size):
+    ser.write(int_to_char(sig_data[i]))
+    d = ser.read()
+    print(char_to_int(d))
