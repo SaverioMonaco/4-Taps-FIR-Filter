f1cbb39ba72b6a43f61f360a27589eb72aaf62d8
Saverio Monaco <saveriomonaco97@gmail.com>
2021-03-05 15:26:15 +0100

    modifing py

---
+++ src/script.py
@@ -0,0 +1,25 @@
+import serial
+
+ser = serial.Serial('/dev/ttyUSB21', baudrate=115200) # open serial port
+print(ser.name) # check which port was really used
+
+def to_2(num):
+    if num <= 127:
+        return num
+    elif num > 127:
+        return num - 256
+
+with open("signal.txt") as f, open("output.txt", "w") as out:
+    signal = [int(line.rstrip()) for line in f]
+
+    for sig in signal:
+        ser.write(chr(sig))
+
+        d = ord(ser.read())
+        print(d)
+        res = str(to_2(d))
+        out.write(res + '\n')
+
+    f.close()
+    out.close()
+#ser.close() # close port
